15b428752a8a4b8b0118d4353fb5d13658371685
samuelguegon89 <samuelguegongithub@gmail.com>
2014-03-21 19:26:28 +0100

    Ejercicio 3 Resuelto

---
+++ ejer3.py
@@ -0,0 +1,7 @@
+usuario1=str(raw_input("Introduzca usuario: "))
+contr=int(raw_input("Introduzca password: "))
+
+if usuario1 == "pepe" and contr == 1234:
+	print "\n" "EL USUARIO Y LA PASSWORD SON CORRECTA"
+else:
+	exit()
