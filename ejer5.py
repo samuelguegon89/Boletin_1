ab5ce225a215190d790a1643819280e73d80cb2a
samuelguegon89 <samuelguegongithub@gmail.com>
2014-03-21 19:28:28 +0100

    Ejercicio 5 Resuelto

---
+++ ejer5.py
@@ -0,0 +1,16 @@
+var1=int(raw_input("Introduzca el primer numero: "))
+var2=int(raw_input("Introduzca el segundo numero: "))
+var3=int(raw_input("Introduzca el tercer numero: "))
+var4=int(raw_input("Introduzca el cuarto numero: "))
+var5=int(raw_input("Introduzca el quinto numero: "))
+
+minimo=min(var1,var2,var3,var4,var5)
+maximo=max(var1,var2,var3,var4,var5)
+promedio=(var1 + var2 + var3 + var4 + var5) / 5
+
+print "\n""El minimo es", minimo
+print "El maximo es", maximo
+print "El promedio es", promedio
+
+
+
